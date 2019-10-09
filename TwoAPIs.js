555f830ce9cb691dd9411b79870e2229deafc485
trinabal <kmb6784@psu.edu>
2019-10-09 10:09:02 -0400

    Added Nutrition API

--- TwoAPIs.js
+++ TwoAPIs.js
@@ -1,3 +1,22 @@
+// var data = null;
+//
+// var xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
+//
+// xhr.addEventListener("readystatechange", function () {
+// 	if (this.readyState === this.DONE) {
+//
+// 		console.log(this.responseText);
+// 	}
+// });
+//
+// xhr.open("GET", "https://recipe-puppy.p.rapidapi.com/?p=1&i=onions%2Cgarlic&q=omelet");
+// xhr.setRequestHeader("x-rapidapi-host", "recipe-puppy.p.rapidapi.com");
+// xhr.setRequestHeader("x-rapidapi-key", "636f281abemshf2f10570570279dp10170ajsn3c5b24fe3431");
+//
+// xhr.send(data);
+
+
 var data = null;
 
 var xhr = new XMLHttpRequest();
@@ -5,13 +24,12 @@ xhr.withCredentials = true;
 
 xhr.addEventListener("readystatechange", function () {
 	if (this.readyState === this.DONE) {
-    
 		console.log(this.responseText);
 	}
 });
 
-xhr.open("GET", "https://recipe-puppy.p.rapidapi.com/?p=1&i=onions%2Cgarlic&q=omelet");
-xhr.setRequestHeader("x-rapidapi-host", "recipe-puppy.p.rapidapi.com");
+xhr.open("GET", "https://food-calorie-data-search.p.rapidapi.com/api/search?keyword=apple");
+xhr.setRequestHeader("x-rapidapi-host", "food-calorie-data-search.p.rapidapi.com");
 xhr.setRequestHeader("x-rapidapi-key", "636f281abemshf2f10570570279dp10170ajsn3c5b24fe3431");
 
 xhr.send(data);
