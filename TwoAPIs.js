feb588f45e6be40010f5a0963846b5886ae78c66
trinabal <kmb6784@psu.edu>
2019-10-12 21:08:53 -0400

    Fixed input box

--- TwoAPIs.js
+++ TwoAPIs.js
@@ -1,94 +1,29 @@
-//
-// let data = null;
-// let keyword = "apple";
-// let request = new XMLHttpRequest();
-//
-// //Recipe API
-// request.open("GET",  "https://recipe-puppy.p.rapidapi.com/?p=1&i=onions%2Cgarlic&q=omelet" + keyword, true);
-// request.setRequestHeader("x-rapidapi-host", "recipe-puppy.p.rapidapi.com");
-// request.setRequestHeader("x-rapidapi-key", "636f281abemshf2f10570570279dp10170ajsn3c5b24fe3431");
-//
-// //Nutritional API
-// // Change keyword to variable and get from function of submit button
-// request.open("GET", "https://food-calorie-data-search.p.rapidapi.com/api/search?keyword=" + keyword, true);
-// request.setRequestHeader("x-rapidapi-host", "food-calorie-data-search.p.rapidapi.com");
-// request.setRequestHeader("x-rapidapi-key", "636f281abemshf2f10570570279dp10170ajsn3c5b24fe3431");
-//
-// request.onload = function() {
-// 	data = JSON.parse(this.response);
-// 	if (request.status == 200)
-// 	{
-// 		console.log(this.responseText);
-//
-// 		for (let i = 0; i < data.length; i++)
-// 		{
-// 			//Nutritional
-// 			console.log(data[i].shrt_desc);
-//       let food = document.createElement("option");
-//       let foodText = document.createTextNode(data[i].shrt_desc);
-//       food.appendChild(foodText);
-//       document.querySelector("#foodList").appendChild(food);
-// 		}
-// 	}
-// 	else
-// 	{
-// 	 	console.log("ERROR");
-// 	}
-// }
-// request.send();
-
-function radioChoice()
-{
-	let yes = document.querySelector("#yes").checked;
-		if (yes)
-		{
-			testFunction();
-		}
-}
-
-function testFunction()
-{
-		console.log("this function works");
-}
-
 function findRecipe()
 {
 	let data = null;
-	let keyword = "apple";
 	let request = new XMLHttpRequest();
+	let searchWord = document.querySelector("#recipeBox").value;
 
-	request.open("GET",  "https://recipe-puppy.p.rapidapi.com/?p=1&i=" + keyword, true);
+	console.log("Search word: " + searchWord);
+	request.open("GET",  "https://recipe-puppy.p.rapidapi.com/?i=" + searchWord, true);
 	request.setRequestHeader("x-rapidapi-host", "recipe-puppy.p.rapidapi.com");
 	request.setRequestHeader("x-rapidapi-key", "636f281abemshf2f10570570279dp10170ajsn3c5b24fe3431");
 
-	//console.log(data);
 	request.onload = function() {
-
-		//console.log(data);
 		data = JSON.parse(this.response);
-
-		// if (request.status == 200)
-		// {
-		// 	data.forEach(results=>{console.log(results.ingredients);})
-		// }
 		if (request.status == 200)
 		{
 			results = data.results;
-			console.log(results);
+		//	console.log(results);
 			console.log(this.responseText);
 			for (let i = 0; i < results.length; i++)
 			{
 				//Recipe
 				console.log(results[i].ingredients);
 				let recipe = document.createElement("p");
-				// recipe.setAttribute("type", "radio");
-				// recipe.setAttribute("id", "radio");
 				let recipeText = document.createTextNode(results[i].title);
 				recipe.appendChild(recipeText);
-				document.querySelector("#radioButtons").appendChild(recipe);
-				// document.querySelector("#radioButtons").appendChild(recipe)
-
-				//console.log(document.querySelector("eco").checked);
+				document.querySelector("#recipeList").appendChild(recipe);
 			}
 		}
 		else
@@ -100,47 +35,49 @@ function findRecipe()
 }
 
 //Function for selecting a food in dropdown
-function itemSelected()
+function getDropdown()
 {
 	let data = null;
-	let keyword = "apple";
+	let keyword = document.querySelector("#recipeList").value;
 	let request = new XMLHttpRequest();
+
 
 	//Accessing API
 	request.open("GET", "https://food-calorie-data-search.p.rapidapi.com/api/search?keyword=" + keyword, true);
 	request.setRequestHeader("x-rapidapi-host", "food-calorie-data-search.p.rapidapi.com");
 	request.setRequestHeader("x-rapidapi-key", "636f281abemshf2f10570570279dp10170ajsn3c5b24fe3431");
 	request.onload = function() {
-		data = JSON.parse(this.response);
-		if (request.status == 200)
+	data = JSON.parse(this.response);
+	if (request.status == 200)
+	{
+		console.log(this.responseText);
+		for (let i = 0; i < data.length; i++)
 		{
-			console.log(this.responseText);
-
-			for (let i = 0; i < data.length; i++)
-			{
-				//Nutritional
-				console.log(data[i].shrt_desc);
-	      let food = document.createElement("option");
-	      let foodText = document.createTextNode(data[i].shrt_desc);
-	      food.appendChild(foodText);
-	      document.querySelector("#foodList").appendChild(food);
-			}
-		}
-		else
-		{
-		 	console.log("ERROR");
+			//Nutritional
+			console.log(data[i].shrt_desc);
+    	let food = document.createElement("option");
+  		let foodText = document.createTextNode(data[i].shrt_desc);
+	   	food.appendChild(foodText);
+	   	document.querySelector("#foodList").appendChild(food);
 		}
 	}
-	request.send();
+	else
+	{
+		console.log("ERROR");
+	}
+}
+request.send();
+}
 
+//Function for selecting a food in dropdown
+function itemSelected()
+{
 	//Table
   let selectedIndex = document.querySelector("#foodList").selectedIndex;
   let foodName = data[selectedIndex].shrt_desc;
   let calories = data[selectedIndex].energ_kcal;
   let carbs = data[selectedIndex].carbohydrt;
   let sugar = data[selectedIndex].sugar_tot;
-
-  //document.querySelector("#toClear").innerHTML = "";
 
   var newFoodHead = document.createElement("td");
   var foodHeadText = document.createTextNode("Food Name");
